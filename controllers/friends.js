bad86fd3013a0fbee8ca403d3149d9f1193c9bd5
Francisco Mestizo <panchitomestizo@gmail.com>
2025-06-06 22:33:51 +0200

    feat(notification): Send info from backend to open pages on frontend

--- controllers/friends.js
+++ controllers/friends.js
@@ -16,11 +16,11 @@ export class FriendsController {
 
         try {
             const result = await FriendsModel.requestFriendship(userId, friendId);
-
+            
             const receiver = result.receiver
             const pushToken = receiver?.deviceToken
 
-            const senderName = sender.name 
+            const senderName = result.sender.name
 
             if (pushToken) {
                 NotificationService.sendFriendRequestNotification({ to: pushToken, from: senderName })
@@ -60,6 +60,14 @@ export class FriendsController {
 
         try {
             const result = await FriendsModel.acceptFriendRequest(userId, friendId);
+            
+            const pushToken = result.friend?.deviceToken;
+            const senderName = result.user?.name;
+
+            if (pushToken) {
+                NotificationService.sendAcceptedFriendRequestNotification({ to: pushToken, from: senderName })
+            }
+
             res.status(200).send({ result });
         } catch (err) {
             console.error(err);
--- services/NotificationService.js
+++ services/NotificationService.js
@@ -1,12 +1,40 @@
+import apn from 'apn';
 import { apnProvider } from "../drivers/apnsNotifications.js";
 
 export class NotificationService {
     static async sendFriendRequestNotification({ to, from }) {
         const notification = new apn.Notification({
-            alert: `${from} sent you a friend request!`,
-            sound: 'default',
+            alert: {
+                title: "Let's tie some knots!",
+                body: `${from} sent you a friend request`,
+            },
             topic: 'com.panchito.Slice',
         });
+        // notification.sound = 'mannagg.caf'
+        notification.payload = {
+            type: "FRIEND_REQUEST"
+        }
+
+        try {
+            let response = await apnProvider.send(notification, to);
+            console.log('APNs response:', response);
+        } catch (err) {
+            console.error('APNs send error:', err);
+        }
+    }
+
+    static async sendAcceptedFriendRequestNotification({ to, from }) {
+        const notification = new apn.Notification({
+            alert: {
+                title: "Beautiful knot!",
+                body: `${from} accepted your friend request`,
+            },
+            topic: 'com.panchito.Slice',
+        });
+        // notification.sound = 'mannagg.caf'
+        notification.payload = {
+            type: "FRIEND_REQUEST_ACCEPTED"
+        }
 
         try {
             let response = await apnProvider.send(notification, to);
