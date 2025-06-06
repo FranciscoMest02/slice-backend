3035806090f143fe95bf2cb0a1a9a61f767a0a11
Francisco Mestizo <panchitomestizo@gmail.com>
2025-06-06 10:23:47 +0200

    fix(crons-time): Crons set to run at right time

--- jobs/pairingCron.js
+++ jobs/pairingCron.js
@@ -2,7 +2,7 @@ import cron from 'node-cron';
 import { runPairingJob } from '../services/runPairingJob.js';
 
 // Runs every day at 1:00 AM server time (9AM UTC - Italy time)
-cron.schedule('22 10 * * *', async () => {
+cron.schedule('00 9 * * *', async () => {
   console.log("⏰ Running scheduled pairing + notification job...");
   try {
     await runPairingJob();
